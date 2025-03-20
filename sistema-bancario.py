df0ad1ecf22c27a8d45102e410cdb33abb5c3ac3
joaopedrolour3nc <joaopedro08moura@gmail.com>
2025-03-20 10:13:51 -0300

    V1.1 com resoluções de Bugs

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -1,5 +1,5 @@
 menu = '''
-    Seja Bem vindo ao TecBanc
+    Seja Bem vindo ao TecBank
     [D] para Depositar
     [S] para Sacar
     [E] para Estrato da Conta
@@ -18,31 +18,36 @@ while True:
 
     if opcao == 'D' or opcao == 'd':
         valor = int(input('Digite o valor para Depósito: '))
-        valor_int = valor
-        if valor_int > 0:
-            saldo += valor_int
+        if valor > 0:
+            saldo += valor
             print(f'Seu saldo atual é de R${saldo:.2f}')
+            extrato += f'Depósito: R$ {valor:.2f}\n'
         else:
             print('Valor inválido')
     elif opcao == "S" or opcao == "s":
         saque = int(input('Digite o valor que deseja sacar:'))
         if saque <= saldo and numero_saques < LIMITE_SAQUES and saque < limite:
             print ('Saque realizado com sucesso')
+            print(f'\nSaque: R${valor:.2f}')
             saldo -= saque
             numero_saques += 1
-            print(f'Segue seu saldo atualiado: R${saldo:.2f}')
             print(f'Você fez {numero_saques} saques')
+            extrato += f'Saque: R$ {saque:.2f}\n'
+            print(f'Segue seu saldo atualiado: R${saldo:.2f}')
         elif saldo < saque:
             print('Saldo insuficiente')
-        elif numero_saques == LIMITE_SAQUES:
+        elif numero_saques >= LIMITE_SAQUES:
             print('Limite de saques diários atingido')
         else:
             print('Valor de saque inválido')
     elif opcao == 'E' or opcao == 'e':
-        extrato = saldo
-        print(f'Seu saldo é de R${extrato:.2f}')
+        print('================ EXTRATO ==============')
+        print('Não foram realizadas movimentações.' if not extrato else extrato)
+        print(f'\nSaldo: R${saldo:.2f}')
+        # print(f'Seu saldo é de R${saldo:.2f}')
         print(f'Sua quantidade de Saques é de {numero_saques}')
         print(f'Seu limite de saque é de R${limite:.2f}')
+        print('======================================')
     elif opcao == 'Q' or opcao == 'q':
         break
     else:
